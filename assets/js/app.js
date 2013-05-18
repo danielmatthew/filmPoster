bc86a7904e4910fd4c8cf113d028a20967aea637
Daniel <hello@danmatthew.co.uk>
2013-05-18 18:48:59 +0100

    Displays an image for each gallery element
    Thanks Ben <3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,108 +1,79 @@
 (function() {
-	'use strict';
+    'use strict';
 
-	var URL = 'http://api.themoviedb.org/3/';
-	var API = '20c37a7bb085a194a8781df8dd193ac1';
+    var URL = 'http://api.themoviedb.org/3/',
+        API = '20c37a7bb085a194a8781df8dd193ac1',
+        baseURL = localStorage.getItem('url'),
+        size = localStorage.getItem('size');
 
-	var baseURL = localStorage.getItem('url'),
-		size = localStorage.getItem('size');
+    window.addEventListener('load', windowLoadHandler, false);
 
-	window.addEventListener('load', windowLoadHandler, false);
+    function windowLoadHandler() {
+        getConfig();
+        getFilmTitles();
+    }
 
-	function windowLoadHandler() {
-		addEventListeners();
-		getConfig();
-		//getFilmID('Pitch Perfect');
-		// search('Crimson Tide');
-		getFilmTitles();
-	}
+    function addEventListeners() {
+        document.getElementById('getPoster').addEventListener('click', getPoster, false);
+    }
 
-	function addEventListeners() {
-		document.getElementById('getPoster').addEventListener('click', getPoster, false);
-	}
+    function getConfig() {
+        if (!localStorage.getItem('url')){
+            ajax(URL + 'configuration?api_key=' + API, loadConfig);       
+        }
+    }
 
-	function getConfig() {
-		if (!localStorage.getItem('url')){
-			ajax(URL + 'configuration?api_key=' + API, loadConfig);			
-		}
-	}
+    function loadConfig(content) {
+        console.log(content);
 
-	function loadConfig(content) {
-		console.log(content);
+        var baseURL = content['images']['base_url'];
+        var size = content['images']['poster_sizes'][3];
 
-		var baseURL = content['images']['base_url'];
-		var size = content['images']['poster_sizes'][3];
+        localStorage.setItem('url', baseURL);
+        localStorage.setItem('size', size);
+    }
 
-		localStorage.setItem('url', baseURL);
-		localStorage.setItem('size', size);
-	}
+    // Extracts film titles from element IDs
+    function getFilmTitles() {
+        var posters = document.getElementsByClassName('poster');
+        for (var i = 0; i < posters.length; i++) {
+            search(posters[i], posters[i].id);
+        }
+    }
 
-	// Extracts film titles from element IDs
-	function getFilmTitles() {
-		// Search document for all poster elements
-		var posters = document.getElementsByClassName('poster');
-		
-		// Get id from each poster element
-		for (var i = 0; i < posters.length; i++) {
-			search(posters[i].id);
-		}
-	}
+    function search(poster, filmTitle) {
+        return ajax(URL + 'search/movie?query=' + encodeURI(filmTitle) + '&api_key=' + API, function (content) {
+            getPoster(content, poster);
+        });
+    }
 
-	function search(filmTitle) {
-		ajax(URL + 'search/movie?query=' + encodeURI(filmTitle) + '&api_key=' + API, function(content){
-			console.log(content['results'][0]['poster_path']);
-			var filmID = content['results'][0]['id'];
-			var filmPosterPath = content['results'][0]['poster_path'];
+    function getPoster(content, poster) {
+        var filmPosterPath = content['results'][0]['poster_path'];
+        var imgPath = baseURL + size + filmPosterPath;
+        outputPoster(imgPath, poster);
+    }
 
-			// Build Poster URL
-			var imgPath = baseURL + size + filmPosterPath;
-	
-			drawPosterNew(imgPath);
-		});
-	}
+    function outputPoster(imgPath, poster) {
+        var img = document.createElement('img');
+        img.setAttribute('src', imgPath);
+        poster.appendChild(img);
+    }
 
-	function getFilmID(filmTitle) {
-		ajax(URL + 'search/movie?query=' + filmTitle + '&api_key=' + API, function(content){
-			console.log(content);
-		});
-	}
-
-	function getPoster() {
-		ajax(URL + 'movie/114150/images?api_key=' + API , drawPoster);
-	}
-
-	function drawPoster(content) {
-		var posterDiv = document.getElementById('poster');
-		var imgPath = content['posters'][0]['file_path'];
-
-		var img = document.createElement('img');
-		img.setAttribute('src', baseURL + size + imgPath);
-
-		posterDiv.appendChild(img);
-	}
-
-	function drawPosterNew(path) {
-		var posterDiv = document.getElementById('poster');
-		var img = document.createElement('img');
-		img.setAttribute('src', path);
-		posterDiv.appendChild(img);
-	}
-
-	// AJAX utility
-	function ajax(url, callback) {
-		var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState == 4) {
-				if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
-					callback(JSON.parse(xhr.responseText));
-					console.log("Request to " + url + " succeeded");
-				}
-				else {
-					console.log("Request to " + url + " failed.");
-				}
-			}
-		};
-		xhr.open("GET", url, true);
-		xhr.send();
-	}
+    // AJAX utility
+    function ajax(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
+                    callback(JSON.parse(xhr.responseText));
+                    console.log("Request to " + url + " succeeded");
+                }
+                else {
+                    console.log("Request to " + url + " failed.");
+                }
+            }
+        };
+        xhr.open("GET", url, false);
+        xhr.send();
+    }
 })();
